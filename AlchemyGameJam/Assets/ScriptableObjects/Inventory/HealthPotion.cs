cdc7af739b40ca5cb2af7e81356ca7b600848467
jordan bentkowski <ah3700@student.jamk.fi>
2026-04-13 19:53:54 +0200

    woop woop art

--- AlchemyGameJam/Assets/ScriptableObjects/Inventory/HealthPotion.cs
+++ AlchemyGameJam/Assets/ScriptableObjects/Inventory/HealthPotion.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace ScriptableObjects.Inventory
 {
     [CreateAssetMenu(menuName = "Game/Potions/Heal Potion")]
-    public class HealPotion : APotion
+    public class HealthPotion : APotion
     {
         public int healAmount;
         public override void Use(GameObject user)
